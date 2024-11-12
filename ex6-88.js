702781d58459453ce4a69873cc7fa1b1809973a3
kobayashiryoryo <167833061+kobayashiryoryo@users.noreply.github.com>
2024-11-12 10:36:24 +0900

    Update ex6-88.js

--- ex6-88.js
+++ ex6-88.js
@@ -1,4 +1,4 @@
 function gazo688()
-{document.getElementById('who688').inneerHTML=
+{document.getElementById('who688').innerHTML=
 '<img src="./image/2fc0d1333d2fe70183ceb5ddbcfa239b.jpeg" alt="オン">';}
 
