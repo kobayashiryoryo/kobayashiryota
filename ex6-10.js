3817864b9bc56ce05879d36b93620a90dbe2ef72
kobayashiryoryo <167833061+kobayashiryoryo@users.noreply.github.com>
2024-11-25 20:28:46 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,7 +1,7 @@
 function imgchange1(){document.getElementById('logo').src='./image/abm00047230 (1).jpg';}
 function imgchange2(){document.getElementById('logo').src='./image/png-transparent-josai-university-hd-logo.png';}
-function imgchange3(){document.getElementById('logo').src='./image/abm00047230 (1).jpg';}
-function imgchange4(){document.getElementById('logo').src='./image/png-transparent-josai-university-hd-logo.png';}
+function imgchange3(){document.getElementById('log').src='./image/abm00047230 (1).jpg';}
+function imgchange4(){document.getElementById('log').src='./image/png-transparent-josai-university-hd-logo.png';}
 
 
 
