fb4b532cb2439ffbfbe412d0fbea983bc0b317da
kobayashiryoryo <167833061+kobayashiryoryo@users.noreply.github.com>
2024-11-25 20:12:42 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,1 +1,7 @@
-function imgchange1(){document.getElementById('logo').src='./image/bulbon.gif';
+function imgchange1(){document.getElementById('logo').src='./image/abm00047230 (1).jpg';}
+function imgchange2(){document.getElementById('logo').src='./image/';}
+function imgchange3(){document.getElementById('logo').src='./image/abm00047230 (1).jpg';}
+function imgchange4(){document.getElementById('logo').src='./image/';}
+
+
+
