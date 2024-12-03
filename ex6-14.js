4acd35863d086eed3521339a21484abf59cb8db9
kobayashiryoryo <167833061+kobayashiryoryo@users.noreply.github.com>
2024-12-03 13:10:22 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,1 +1,2 @@
-function hyoji14(){document.getElementById('lo').src ='
+function hyoji14(){document.getElementById('lo').src ='./image/2fc0d1333d2fe70183ceb5ddbcfa239b.jpeg'; setTimeout("hyoji15()",3000);}
+function hyoji15(){document.getElementById('lo').src ='
