7582e7c5247de0911d9f6a6fba2f766c7a1ec546
kobayashiryoryo <167833061+kobayashiryoryo@users.noreply.github.com>
2024-12-03 11:17:43 +0900

    Update ex6-13.js

--- ex6-13.js
+++ ex6-13.js
@@ -1,6 +1,3 @@
-...
-function mes(){alert("3秒経ちました！");}
-
 function hyoji13(){var str13="一文字ずつ表示します。";
                    var cnt13=document.timer13.moji13.value.length;
                    if(cnt13<11){document.timer13.moji13.value=str13.substr(0,cnt13+1);}
