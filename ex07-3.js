72f84a24fc5957cb87f2f78a3fea2ff330954895
kobayashiryoryo <167833061+kobayashiryoryo@users.noreply.github.com>
2024-12-10 12:59:22 +0900

    Update ex07-3.js

--- ex07-3.js
+++ ex07-3.js
@@ -1,3 +1,3 @@
-function div(){var x=document.getElementById('input1').value;
-               var y=document.getElementById('input2').value;
-alert(x/Number(Math.pow(y,2));}
+function div(){var x=document.getElementById('input3').value;
+               var y=document.getElementById('input4').value;
+alert(x/Math.pow(y,2));}
