717b733d21ed83b6d98de4f1bbfe7e45991708cb
kobayashiryoryo <167833061+kobayashiryoryo@users.noreply.github.com>
2024-12-17 13:20:24 +0900

    Update ex7-8.js

--- ex7-8.js
+++ ex7-8.js
@@ -1,1 +1,1 @@
-function nazo78(){if(document.getElementById('answer78').value='夢'){alert('あたり！');}else{alert('はずれ！');}}
+function nazo78(){if(document.getElementById('answer78').value=='夢'){alert('あたり！');}else{alert('はずれ！');}}
