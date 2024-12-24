f28264c562dcc21bab4e4b8f3632d4cd1900982a
kobayashiryoryo <167833061+kobayashiryoryo@users.noreply.github.com>
2024-12-24 10:33:39 +0900

    Update ex7-10.js

--- ex7-10.js
+++ ex7-10.js
@@ -1,3 +1,3 @@
 function div710(){var x=document.getElementById('input5').value;
                   var y=document.getElementById('input6').value;
-                  alert(x/Math.pow(y,2));}else if
+                  alert(x/Math.pow(y,2));}else if(document.getElementById('x/Math.pow(y,2)').value<
