a5595fd46a08f9b17cf1fcb36b0c260ad0a542d3
kobayashiryoryo <167833061+kobayashiryoryo@users.noreply.github.com>
2025-01-24 16:14:19 +0900

    Update ex7-11.js

--- ex7-11.js
+++ ex7-11.js
@@ -1,1 +1,1 @@
-
+function uruu711(){var x=document.getElementById('year711').value; if(((x % Number(100) == '0') && !(x % Number(400) == '0')) || !(x % Number(4) == '0')) {alert('平年')} else{alert('うるう年')}}
