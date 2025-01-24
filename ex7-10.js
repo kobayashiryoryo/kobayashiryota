54e5977c26b3c25baf49411d013a022a191fad66
kobayashiryoryo <167833061+kobayashiryoryo@users.noreply.github.com>
2025-01-24 16:00:33 +0900

    Update ex7-10.js

--- ex7-10.js
+++ ex7-10.js
@@ -1,3 +1,3 @@
-function div710(){var x=document.getElementById('input5').value;
-                  var y=document.getElementById('input6').value;
-                  alert(x/Math.pow(y,2));}else if(document.getElementById('x/Math.pow(y,2)').value<18.5){alert('低体重')}else if(document.getElementById('x/Math.pow(y,2)').value<25){alert('普通')}else{alert('肥満');}}
+function div710(){var x=document.getElementById('input7-1').value;
+                  var y=document.getElementById('input7-2').value;
+                  alert(x/Math.pow(y,2)); if(x/Math.pow(y,2)< '18.5'){alert('低体重');} else if(x/Math.pow(y,2) > '25'){alert('肥満')} else{alert('普通')}}  
